1d6f397945db775956f3745fec6e8f1148bfea32
jstarts86 <jstarts86@gmail.com>
2023-09-01 13:54:00 +0900

    assingment 1 start

--- test/main.cpp
+++ test/main.cpp
@@ -9,7 +9,7 @@ int main() {
     Mat image;
     image = imread("Lena.png", IMREAD_COLOR);
     if (image.empty()) {
-        cout << "COuld not open or find the image" << endl;
+        cout << "Could not open or find the image" << endl;
         return -1;
     }
 
