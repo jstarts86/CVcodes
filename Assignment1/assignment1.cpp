f84e4e61e1d3221731c46f9e08743f90de0caf42
John Song <jstarts86@gmail.com>
2023-09-04 00:56:19 +0900

    Assignment 1 complete

---
+++ Assignment1/assignment1.cpp
@@ -0,0 +1,52 @@
+#include  <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+
+int main() {
+    Mat frame;
+    VideoCapture cap;
+    int fps;
+    int delay;
+    if (cap.open("background.mp4") == 0 ){
+        cout << "no such file!" << endl;
+        waitKey(0);
+    }
+
+    fps = cap.get(CAP_PROP_FPS); 
+    delay = 1000 /fps;
+
+    for(int i = 0; i <= 72; i++){
+        cap >> frame;
+        
+        if (frame.empty()) {
+            cout << "end of video" << endl;
+            break;
+        }
+        imshow("video", frame);
+
+        waitKey(delay);
+    }
+
+    double total_frame = cap.get(CAP_PROP_FRAME_COUNT);
+    double time_in_msec = cap.get(CAP_PROP_POS_MSEC);
+    int current_frame = cap.get(CAP_PROP_POS_FRAMES);
+    printf("fps is %d \ntotal frame count is %f\ntime in msec is %f\n Current frame is %d", fps,total_frame,time_in_msec,current_frame);
+
+    // while(1) {
+    //     cap >> frame;
+
+    //     if (frame.empty()) {
+    //         cout << "end of video" << endl;
+    //         break;
+    //     }
+    //     imshow("video", frame);
+
+    //     waitKey(delay);
+    // }
+
+
+
+}
---
+++ Assignment1/excercise1.cpp
@@ -0,0 +1,10 @@
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    Mat test(480,640,CV_8UC3, Scalar(0,255,0));
+    
+}
